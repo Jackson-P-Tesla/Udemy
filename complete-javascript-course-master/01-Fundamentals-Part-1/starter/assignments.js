c797c3c77b8b06c56355280d953559a10bdd30a1
Jackson-P-Tesla <jackson.c.parker@gmail.com>
2024-04-08 18:29:08 -0400

    chore: adding the beginning of the assignments stuff

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,73 @@
+console.log('hello World');
+
+const country = 'Canada';
+const continent = 'North America';
+let population = 35;
+
+console.log(country, continent, population);
+
+const isIsland = false;
+let language = 'English';
+
+console.log(isIsland, population, country, language);
+
+let halfPopulation = population / 2;
+
+console.log(population+1);
+
+let finlandPop = 60;
+
+if (population > finlandPop){
+    console.log(`My country of ${country} has more population than Finland`);
+}
+
+if (population < 33){
+    console.log(true);
+} else {
+    console.log(`${country}'s population is ${population - 33} million above average`);
+}
+
+let description = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+
+console.log(description);
+
+console.log('9' - '5'); // -> error
+console.log('19' - '13' + '17'); // -> error
+console.log('19' - '13' + 17); // -> 17
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 14
+
+// let numNeighbors = Number(prompt('How many neighbour countries does your country have?'));
+//
+//
+//
+// if (numNeighbors === 1){
+//     console.log('Only 1 border!');
+// } else if(numNeighbors > 1){
+//     console.log('More than 1 border.');
+// } else {
+//     console.log('No borders')
+// }
+
+if (!isIsland && language == 'English' && population < 50 ){
+    console.log(`You should live in ${country} :).`)
+}
+
+switch (language){
+    case 'chinese':
+    case 'mandarin':
+        console.log('Most number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+}
